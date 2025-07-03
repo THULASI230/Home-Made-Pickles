83a6707a6ea2dcc2c8a0c33a799e904d0209d54c
THULASI230 <tthulasireddy2305@gmail.com>
2025-07-03 13:47:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -225,7 +225,6 @@ def internal_error(e):
 # -------------------- Run --------------------
 
 if __name__ == '__main__':
-    app.run(debug=True)
     app.run(debug=True, host='0.0.0.0', port=5000)
 
       
